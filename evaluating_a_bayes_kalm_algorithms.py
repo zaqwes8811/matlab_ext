3c5d3556aa49f9e35918f7005948257523aac3b2
zaqwes8811 <igor.a.lugansky@gmail.com>
2016-03-13 19:07:20 +0300

    add ev. files

---
+++ evaluating_a_bayes_kalm_algorithms.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 13 18:59:02 2016
+
+@author: zaqwes
+"""
+
+# "Do not trust the filter’s covariance matrix to tell you 
+# if the filter is performing well!"
+#
+# "Again, the residual plot tells the story."
