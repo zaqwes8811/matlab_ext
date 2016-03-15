38cb8d3d9a1b85b6ad8596ca7513a694816fe39c
Your Name <you@example.com>
2016-03-15 13:57:55 +0300

    save

--- r_ext.py
+++ r_ext.py
@@ -10,46 +10,51 @@ def flatmap( vs, f ):
 	return r
 
 def try_parse(string, fail=None):
-	try:
-		return float(string)
-	except Exception:
-		return fail
+    try:       
+        a = float(string)        
+        return a 
+    except Exception:
+        return fail
 
-def write_csv_( fn, titles, arr ):
-	# http://stackoverflow.com/questions/6081008/dump-a-numpy-array-into-a-csv-file
-	title = ','.join( titles )
-	# with open( fn, 'a') as the_file:
-	# 	the_file.write(title+'\n')
-
-	np.savetxt( fn, arr, 
-		comments="",
-		header=title, fmt='%10.5f',  delimiter=',') 
+def write_csv_spec( fn, arr, titles=None ):
+    # http://stackoverflow.com/questions/6081008/dump-a-numpy-array-into-a-csv-file
+    if titles:
+        title = ','.join( titles )
+        
+        np.savetxt( fn, arr, 
+                   comments="",
+                   header=title, fmt='%10.5f',  delimiter=',') 
+    else:
+        np.savetxt( fn, arr, 
+                   comments="",
+                   fmt='%10.5f',  delimiter=',') 
 
 
 def read_csv_spec( fn, sep=',' ):
-	""" 
-		Args: "f:a:10,b:90, 18.9:67"
+    """ 
+    		Args: "f:a:10,b:90, 18.9:67"
+    
+    		Returns: [[10, 90, 18.9, 67]]
+    """
+    lines = []
+    with open( fn ) as f:
+        lines = f.read().splitlines()
+    tmp = []
+    for k in lines:
+        s = k.replace(' ', sep).replace('\t', sep).replace(sep+sep,sep).replace(sep+sep,sep)
+        s = s.replace('=', ':')        
+        a = s.split(',')
+        a = flatmap( a, lambda x: x.split(':'))
+        a = filter( lambda x: try_parse( x ) != None, a )
+        
+        if a:
+            tmp.append( map( lambda x: float(x), a) )
 
-		Returns: [[10, 90, 18.9, 67]]
-	"""
-	with open( fn ) as f:
-		lines = f.read().splitlines()
-
-	tmp = []
-   	for s in lines:
-		s = s.replace(' ', sep).replace('\t', sep).\
-			replace(sep+sep,sep).replace(sep+sep,sep)
-		a = s.split(',')
-		a = flatmap( a, lambda x: x.split(':'))
-		a = filter( lambda x: try_parse( x ), a )
-		if a:
-			tmp.append( map( lambda x: float(x), a) )
-
-	return np.array( tmp )
+    return np.array( tmp )
 
 if __name__=='__main__':
-	data = read_csv_spec( 'tests/r_ext.csv' )
+	data = read_csv_spec( '/home/zaqwes/to_r.csv' )
 	print data
 
-	write_csv_("tests/r_ext_out.csv", ["a", 'b', 'c', 'd', 'e'], data)
-	print read_csv_spec( "tests/r_ext_out.csv")
+#	write_csv_("tests/r_ext_out.csv", ["a", 'b', 'c', 'd', 'e'], data)
+#	print read_csv_spec( "tests/r_ext_out.csv")
