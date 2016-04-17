49ec126d8b715457115bad7c92e1b4973896c96d
zaqwes8811 <igor.a.lugansky@gmail.com>
2016-04-17 14:23:01 +0300

    save

---
+++ pattern_matcher.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import re
+
+if __name__ == '__main__':
+    s1 = 'aaabbbaa'
+    m = re.search(r'^a', s1)
+    print m.groups()
+    
+    # https://habrahabr.ru/post/115825/
+    print re.findall("^a", s1)
+    print re.findall("^a*b+a", s1)
+    print re.findall("^a*b*a", 'aaaaaa')
+    print re.findall("^a*b*a", 'aaaaaa')
+    
+    p = re.compile(r"^a*b{2,4}a", re.DEBUG)
+    print p.findall('aaabbaaabbba')
+    
+    # DFA, NFA, e-NFA - engines
+    # https://msdn.microsoft.com/ru-ru/library/e347654k(v=vs.110).aspx
+    #http://www.intuit.ru/studies/courses/1148/187/lecture/4888
+    # !! https://learn.javascript.ru/regular-expressions-javascript
+    # NP - упоминается, а значит проблемы с произв.
