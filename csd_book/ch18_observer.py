73fc12a849030c1e86a8ae97605c5c4187a4460f
zaqwes8811 <igor.a.lugansky@gmail.com>
2016-04-22 21:30:29 +0300

    save

--- csd_book/ch18_observer.py
+++ csd_book/ch18_observer.py
@@ -5,7 +5,34 @@ Created on Thu Apr 21 22:48:19 2016
 @author: zaqwes
 """
 
+import copy
+
+from control import tf, series
+from control import impulse_response, step_response
+
+from matlab_ext.plotter import *
+from matlab_ext.r_ext import *
 
 if __name__ == '__main__':
+    cls()
+    
     Kt = 1.    
-    Jt = 1000.  # [?]
+    Jt = 1.  # [?]
+    
+    u1 = tf([Kt], [1])
+    u2 = tf([1/Jt], [1])
+    
+    # fixme: как добавить сигнал шума?
+    u12 = series(u1, u2)
+    u3 = tf([1], [1, 0])
+    u4 = copy.deepcopy( u3 )
+    u34 = series(u3, u4)
+    u14 = series(u12, u34)
+    
+    print u14
+
+    ts, xs = step_response(u14)
+    
+    plot(ts, xs)
+    grid()    
+    show()
